6b588b6694d65534df49e231efdd3bbf5f44f653
WinterSunset95 <wintersunset95@gmail.com>
2022-11-20 12:50:11 +0530

    added contact details

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -12,9 +12,12 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { addDoc, query, collection, orderBy, getDocs } from 'firebase/firestore'
 
 const Home = () => {
+
 	const [name, setName] = useState('')
 	const [state, setState] = useState(false)
 	const [uid, setUid] = useState('')
+	const [drop, setDrop] = useState(false)
+
 	const checkAuthState = () => {
 		const auth = getAuth();
 		onAuthStateChanged(auth, (user) => {
@@ -27,6 +30,7 @@ const Home = () => {
 			}
 		});
 	}
+
 	const signIn = () => {
 		const auth = getAuth();
 		const provider = new GoogleAuthProvider();
@@ -37,6 +41,7 @@ const Home = () => {
 			setState(true)
 		})
 	}
+
 	const send = () => {
 		const node = (document.getElementById('input-field') as HTMLInputElement)
 		const msg = node.value
@@ -51,25 +56,47 @@ const Home = () => {
 		}) : console.log(msg)
 		node.value = ''
 	}
+
 	const check = (event:any) =>{
 		event.keyCode == 13 ? send() : null
 	}
+
 	const messagesRef = query(collection(db, 'global'), orderBy('timestamp'))
 	const [messages] = useCollectionData(messagesRef)
+
 	useEffect(() => {
 		checkAuthState()
 	}, [name])
+
 	useEffect(() => {
 		const node = document.getElementById('messages')
 		node!.scrollTop = node!.scrollHeight
 	}, [messages])
+
 	return (
 		<main className="sticky top-0 left-0 w-full h-screen max-w-[800px] m-auto flex flex-col border-2 border-black bg-[rgba(50,50,50,0.5)]">
-			<div className="p-4 flex justify-between bg-black text-white MontAlt">
+			<div className="p-4 flex justify-between bg-black text-white MontAlt relative">
 				<span>FireChat</span>
 				<span>{name}</span>
-				<div>
-					<i className="fa-solid fa-right-to-bracket"></i>
+				<div className={`${drop ? 'hidden' : ''}`} onClick={() => setDrop(true)}>
+					<i className={`fa-solid fa-bars`}></i>
+				</div>
+				<div className={`${drop ? '' : 'hidden'} relative`}>
+					<i className={`fa-solid fa-circle-xmark`} onClick={() => setDrop(false)}></i>
+				</div>
+				<div className={`${drop ? 'h-40' : 'h-0'} absolute bottom-0 right-4 translate-y-full overflow-hidden duration-100 flex flex-col justify-around items-center z-50`}>
+					<a href="https://github.com/WinterSunset95/FireChat" target="_blank">
+						<i className="fa-brands fa-github fa-2x m-1"></i>
+					</a>
+					<a href="https://facebook.com/autumntowinter"  target="_blank">
+						<i className="fa-brands fa-facebook fa-2x m-1"></i>
+					</a>
+					<a href="https://reddit.com/u/WallaceThiago95" target="_blank">
+						<i className="fa-brands fa-reddit fa-2x m-1"></i>
+					</a>
+					<a href="https://wintersunset-portfolio.vercel.app" target="_blank">
+						<i className="fa-solid fa-house fa-xl m-1"></i>
+					</a>
 				</div>
 			</div>
 			<section className="grow w-full flex flex-col overflow-y-scroll" id="messages">
