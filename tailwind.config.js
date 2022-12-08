10981915c6d7ad5ae4aee340152b92f998342996
WinterSunset95 <wintersunset95@gmail.com>
2022-12-08 18:58:03 +0530

    switched to nextjs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-		"./src/**/*.{js,jsx,ts,tsx}",
-	],
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
