af1743b2bf9ebba93f3abd2ccc95ad550ce596c8
WinterSunset95 <wintersunset95@gmail.com>
2022-12-12 12:43:31 +0530

    added home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,16 @@
+
+
+
+
+
+
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+import { useState, useEffect, useContext } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
