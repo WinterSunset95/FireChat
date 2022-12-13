417f59edbec02fc624998d44cd5cb204417848f0
WinterSunset95 <wintersunset95@gmail.com>
2022-12-13 20:37:50 +0530

    private chat implemented

--- components/User.tsx
+++ components/User.tsx
@@ -3,40 +3,48 @@
 
 
 import styles from '../styles/Home.module.css'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useContext} from 'react'
+import AppContext from '../AppContext'
 import { db } from '../firebase/Firebase'
 import { collection, getDoc, doc, query, orderBy, addDoc } from 'firebase/firestore'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPaperPlane, faXmarkCircle, faBars, faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faPaperPlane, faXmarkCircle, faBars, faHouse, faCircleArrowLeft } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faFacebook, faReddit } from '@fortawesome/free-brands-svg-icons'
 
 const User = ({usernav, setUsernav}:any) => {
+	const {logOut} = useContext(AppContext)
 	// getting the users
 	const usersRef = query(collection(db, 'users'))
 	const users = useCollectionData(usersRef)[0]
 	return (
-				<section className={`flex flex-col ${styles.usersList} h-full bg-[rgba(50,50,50,1)] ${styles.users} ${usernav ? 'p-4 w-full' : 'p-0 w-0'} md:w-full md:p-4 outline outline-2 outline-black`}>
-					<div>
-						<div className={`md:hidden ${usernav ? '' : 'hidden'} flex w-full flex-row justify-end`}>
-							<FontAwesomeIcon icon={faXmarkCircle} onClick={() => setUsernav(false)} className="md:hidden fa-xl"/>
-						</div>
-						{
-							users && users.map((user) => {
-								return (
-									<div key={user.id} className="flex flex-row justify-center items-center w-fit mb-1">
-										<img src={user.picture} alt="" className="rounded-full mr-1 w-12"/>
-										<div className="p-2 mb-1 rounded-md bg-[rgba(100,100,100,0.5)] hover:bg-white w-fit h-fit text-white">
-											<a href={`/chat/${user.uid}`}>{user.name}</a>
-										</div>
-									</div>
-								)
-							})
-						}
+		<section className={`flex flex-col justify-between ${styles.usersList} h-full bg-[rgba(50,50,50,1)] ${styles.users} ${usernav ? 'p-4 w-full' : 'p-0 w-0'} md:w-full md:p-4 outline outline-2 outline-black`}>
+			<div>
+				<div className={`md:hidden ${usernav ? '' : 'hidden'} flex w-full flex-row justify-end`}>
+					<FontAwesomeIcon icon={faXmarkCircle} onClick={() => setUsernav(false)} className="md:hidden fa-xl"/>
+				</div>
+				<div className="flex flex-row justify-center items-center w-fit mb-2">
+					<FontAwesomeIcon icon={faCircleArrowLeft} className="fa-3x mr-1"/>
+					<div className="p-2 mb-1 rounded-md bg-[rgba(100,100,100,0.5)] hover:bg-white w-fit h-fit text-white">
+						<a href={`/`}>Global Chat</a>
 					</div>
-					<div className="px-4 py-2 bg-blue-400">Log Out</div>
-				</section>
+				</div>
+				{
+					users && users.map((user) => {
+						return (
+							<div key={user.id} className="flex flex-row justify-center items-center w-fit mb-2">
+								<img src={user.picture ? user.picture : "https://avatars.dicebear.com/api/bottts/" + user.name + ".svg"} alt="../public/vercel.svg" className="rounded-full mr-1 w-12 border border-2"/>
+								<div className="p-2 mb-1 rounded-md bg-[rgba(100,100,100,0.5)] hover:bg-white w-fit h-fit text-white">
+									<a href={`/chat/${user.uid}`}>{user.name}</a>
+								</div>
+							</div>
+						)
+					})
+				}
+			</div>
+			<div className="px-4 py-2 bg-blue-400 w-fit rounded-lg" onClick={() => logOut()}>Log Out</div>
+		</section>
 	)
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,50 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import { useState, useEffect, useContext } from 'react'
+import AppContext from '../AppContext'
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut } from 'firebase/auth'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [name, setName] = useState('')
+	const [state, setState] = useState(false)
+	const [uid, setUid] = useState('')
+
+	const checkAuthState = () => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setState(true)
+				setName(user.displayName!)
+				setUid(user.uid!)
+			} else {
+				setState(false)
+			}
+		});
+	}
+
+	const signIn = () => {
+		const auth = getAuth();
+		const provider = new GoogleAuthProvider();
+		signInWithPopup(auth, provider)
+		.then((result) => {
+			setName(auth.currentUser!.displayName!)
+			setUid(auth.currentUser!.uid)
+			setState(true)
+		})
+	}
+
+	const logOut = () => {
+		const auth = getAuth()
+		signOut(auth)
+	}
+
+	useEffect(() => {
+		checkAuthState()
+	}, [name])
+
+	return (
+		<AppContext.Provider value={{ name, setName, state, setState, uid, setUid, signIn, logOut }}>
+			<Component {...pageProps} />
+		</AppContext.Provider>
+	)
 }
